02727c8e35f170d697e468a93d0e224e41974703
Chris van der Loo <chris.vanderloo@icloud.com>
2026-09-07 16:59:36 -0400

    bundle @atproto deps

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -9,5 +9,8 @@ export default defineConfig({
     "./src/components/handle-field-element.ts",
   ],
   copy: [{ from: "src/components/*.astro", to: "dist/components" }],
-  deps: { neverBundle: ["at-astro:config"] },
+  deps: {
+    neverBundle: ["at-astro:config"],
+    alwaysBundle: ["@atproto/jwk-webcrypto", "@atproto/oauth-client"],
+  },
 })
